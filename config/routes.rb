fd7dc238a48b802f1021994a88f53571477b4f10
Will Tinney <willtinney1@gmail.com>
2017-01-15 19:23:44 +0000

    Created VouchersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'vouchers/index'
+
+  get 'vouchers/show'
+
+  get 'vouchers/new'
+
+  get 'vouchers/create'
+
+  get 'vouchers/edit'
+
+  get 'vouchers/update'
+
+  get 'vouchers/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
