fc1455b2a4e832e37db67b3ecccd0cff4ca07a2e
Will Tinney <willtinney1@gmail.com>
2017-01-21 12:12:54 +0000

    Created integration tests for home/boucher index page

---
+++ test/integration/home_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class HomeTest < ActionDispatch::IntegrationTest
+  test "loads correctly" do
+    visit "/"
+    assert_equal 200, page.status_code
+  end
+
+  test "index_loads_correct_number_of_vouchers" do
+    visit "/"
+    assert page.has_selector?('.voucher', count: Voucher.count)
+    save_and_open_screenshot
+  end
+end
