56d600a499422a3ab18655ab0252a409ef339a85
Will Tinney <willtinney1@gmail.com>
2017-01-21 09:25:41 +0000

    Assigned root page to Vouchers index, added view html/css for index

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,22 +1,7 @@
 class VouchersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+    @vouchers = Voucher.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
 Rails.application.routes.draw do
-  get 'vouchers/index'
-
-  get 'vouchers/show'
-
-  get 'vouchers/new'
-
-  get 'vouchers/create'
-
-  get 'vouchers/edit'
-
-  get 'vouchers/update'
-
-  get 'vouchers/destroy'
 
   devise_for :users
-  root to: 'pages#home'
+  root to: 'vouchers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
